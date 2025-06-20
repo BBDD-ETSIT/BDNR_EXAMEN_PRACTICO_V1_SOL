7187981485007663c06a3ab7a7e16c3eccab7d89
J. Fernando Sánchez <jf.sanchez@upm.es>
2025-06-20 17:29:43 +0200

    actualizar solución knowledge graph

--- flaskr/knowledge_graph.py
+++ flaskr/knowledge_graph.py
@@ -1,24 +1,60 @@
 import json
+import time
+from sparql import query
 
 def get_uri(company) -> str:
     """Apartado A: Mostrar la URI de la productura dada"""
-    return "DESCONOCIDA"
+    txt = f'''
+    SELECT ?uri
+    WHERE {{
+        ?uri wdt:P31 wd:Q1762059 ;
+             rdfs:label "{ company["name"] }"@en .
 
-def movies(company) -> dict[str, str]:
+    }}
+    LIMIT 1
+    '''
+    q = query(txt)
+    return q[0]['uri'] if q else None
+
+def movies(uri) -> dict[str, str]:
     """Apartado A: Devolver películas producidas por esta productora como un diccionario URI -> título de la película en castellano"""
-    return { "http://example.com/inception": "Origen"}
+    movies = query(f'''
+    SELECT ?uri ?name
+    WHERE {{
+       ?uri wdt:P272 <{uri}>;
+            rdfs:label ?name .
+        FILTER(LANG(?name) = "en")
+    }}
+    LIMIT 5
+   ''')
+    return { m["uri"]: m["name"] for m in movies}
 
 def other_examples() -> dict[str, str]:
     """Apartado D: Devolver otros ejemplos de productoras, en forma de diccionario URI -> nombre de la productura en inglés"""
-    return { "http://example.com/ETSITFilms": "ETSIT Films"}
+    res = query('''SELECT ?uri ?name
+    WHERE {
+        ?uri wdt:P31 wd:Q1762059 ;
+             rdfs:label ?name .
+        FILTER(LANG(?name) = "en")
+
+    }
+    LIMIT 20
+    ''')
+    return {r['uri']: r['name'] for r in res}
 
 with open('seeders/production.json') as f:
     productions = json.load(f)
     for p in productions:
-        print("La URI de la productura es:", get_uri(p))
+        print("Productura", p["name"])
+        uri = get_uri(p)
+        if not uri:
+            print("\tNo se pudo encontrar la URI")
+            continue
+        print("\tURI:", uri)
         print("\tPelículas de esta productora:")
-        for (uri, name) in movies(p).items():
+        for (uri, name) in movies(uri).items():
             print(f"\t\t{name} - {uri}")
+        time.sleep(1)
 
 
 print('Otros ejemplos de productoras')
--- flaskr/sparql.py
+++ flaskr/sparql.py
@@ -5,7 +5,7 @@ import requests
 def query(q, endpoint="https://query.wikidata.org/sparql", f='application/json'):
     try:
         params = {'query': q}
-        print(q)
+        #print(q)
         resp = requests.get(endpoint, params=params, headers={'Accept': f})
         bindings = resp.json()["results"]["bindings"]
         return [{k:d["value"] for (k, d) in item.items()} for item in bindings] 
